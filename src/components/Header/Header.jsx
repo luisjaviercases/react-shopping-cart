776088fdeb5c177888895fe85cd5f8e554073f85
Luis Javier Cases <l.cases@cathedralsw.com>
2023-08-30 10:17:49 +0200

    feat(header): create component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import Breadcrumbs from '@/components/Breadcrumbs/Breadcrumbs';
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Header</h1>
+      <Breadcrumbs />
+    </div>
+  );
+};
+
+export default Header;
