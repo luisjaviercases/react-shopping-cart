d77af9ec17a19f782ef1ec210fa1e5d3a243210d
Luis Javier Cases <l.cases@cathedralsw.com>
2023-09-04 13:34:12 +0200

    feat(routes): add product detail route

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,12 +1,14 @@
 import { createBrowserRouter, createRoutesFromElements, Route, Navigate } from 'react-router-dom';
 import App from '@/App';
 import Products from '@/features/products/Products';
+import ProductDetail from '@/features/product-detail/ProductDetail';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route element={<App />}>
       <Route path='' element={<Navigate to='products' />} />
       <Route path='products' element={<Products />} />
+      <Route path='products/product-detail/:id' element={<ProductDetail />} />
     </Route>
   )
 );
