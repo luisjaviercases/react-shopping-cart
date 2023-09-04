0eeecaed52ad042f54954080a4d975800f546e61
Luis Javier Cases <l.cases@cathedralsw.com>
2023-09-04 13:10:34 +0200

    feat(products): add searchbar to page

--- src/features/products/Products.jsx
+++ src/features/products/Products.jsx
@@ -1,12 +1,26 @@
+import { useState } from 'react';
 import { useGetProductsQuery } from '@/services/query/productApi';
+import SearchBar from '@/components/SearchBar/SearchBar';
 
 const Products = () => {
+  const [inputValue, setInputValue] = useState('');
   const { data: products } = useGetProductsQuery();
+
+  const filteredProducts = products?.filter(
+    (product) =>
+      product.model.toLowerCase().includes(inputValue.toLowerCase()) ||
+      product.brand.toLowerCase().includes(inputValue.toLowerCase())
+  );
 
   return (
     <>
-      {products?.map((product) => (
-        <div key={product.id}>{product.id}</div>
+      <SearchBar
+        onChange={(e) => {
+          setInputValue(e.target.value);
+        }}
+      />
+      {filteredProducts?.map((product) => (
+        <div key={product.id}>{product.model}</div>
       ))}
     </>
   );
