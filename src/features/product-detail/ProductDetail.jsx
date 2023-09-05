7c1abf1b0e5d5321d2bff53388e09077049f7381
Luis Javier Cases <l.cases@cathedralsw.com>
2023-09-05 10:12:05 +0200

    feat(product-detail): use get product detail hook

--- src/features/product-detail/ProductDetail.jsx
+++ src/features/product-detail/ProductDetail.jsx
@@ -1,11 +1,14 @@
 import { useParams } from 'react-router-dom';
+import { useGetProductDetailQuery } from '@/services/query/productApi';
 
 const ProductDetail = () => {
   const { id } = useParams();
 
+  const { data: product } = useGetProductDetailQuery(id);
+
   return (
     <>
-      <span>Product detail {id}</span>
+      <span>Current product {product?.model}</span>
     </>
   );
 };
