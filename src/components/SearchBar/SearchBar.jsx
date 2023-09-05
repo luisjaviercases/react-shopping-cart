6ea7cb6790fe43842956841a4fb74b2130546d04
Luis Javier Cases <l.cases@cathedralsw.com>
2023-09-05 17:50:25 +0200

    feat(search-bar): add new props and apply styles to component

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,11 +1,20 @@
 import PropTypes from 'prop-types';
 
-const SearchBar = ({ onChange }) => {
-  return <input type='text' onChange={onChange} placeholder='Search...' />;
+const SearchBar = ({ placeholder, onChange, className }) => {
+  return (
+    <input
+      className={`w-56 text-sm rounded-lg block p-2.5 border-2 ${className}`}
+      type='text'
+      onChange={onChange}
+      placeholder={placeholder}
+    />
+  );
 };
 
 SearchBar.propTypes = {
+  placeholder: PropTypes.string,
   onChange: PropTypes.func,
+  className: PropTypes.string,
 };
 
 export default SearchBar;
