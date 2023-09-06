ffa4316b9838c945fe2d79d8439e2a2352075162
Luis Javier Cases <l.cases@cathedralsw.com>
2023-09-06 19:16:49 +0200

    feat: add configuration files to use testing in project

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ import * as path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/tests/setup.js',
+  },
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
