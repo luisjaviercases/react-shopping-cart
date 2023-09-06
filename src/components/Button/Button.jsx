74f73c473f7fb6f2e3ce544059d5d83fc4606900
Luis Javier Cases <l.cases@cathedralsw.com>
2023-09-06 17:22:59 +0200

    style(button): add tailwind styles to component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,7 +2,12 @@ import PropTypes from 'prop-types';
 
 const Button = ({ children, onClick, className, ...rest }) => {
   return (
-    <button className={className} onClick={onClick} {...rest}>
+    <button
+      className={`${
+        rest.disabled ? 'bg-gray-300 text-gray-500 cursor-not-allowed' : 'bg-blue-500 hover:bg-blue-700 text-white'
+      } text-sm py-2 px-4 rounded ${className ?? ''}`}
+      onClick={onClick}
+      {...rest}>
       {children}
     </button>
   );
