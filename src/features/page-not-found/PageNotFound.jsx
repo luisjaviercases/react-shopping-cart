a97a1a8d81f07bcecf5e6960d7310472c25cd889
Luis Javier Cases <l.cases@cathedralsw.com>
2023-09-07 09:06:23 +0200

    style(page-not-found): add styles to page

--- src/features/page-not-found/PageNotFound.jsx
+++ src/features/page-not-found/PageNotFound.jsx
@@ -1,8 +1,10 @@
 const PageNotFound = () => {
   return (
-    <div>
-      <h1>404 Error</h1>
-      <h1>Page Not Found</h1>
+    <div className='flex items-center justify-center min-h-screen'>
+      <div className='bg-white p-8 rounded-lg shadow-md'>
+        <h1 className='text-4xl font-bold text-red-500 mb-4'>404 Error</h1>
+        <h2 className='text-xl text-gray-800'>Page Not Found</h2>
+      </div>
     </div>
   );
 };
