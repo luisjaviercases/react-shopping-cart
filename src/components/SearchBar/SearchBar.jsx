902d17d0bcf512a44bf2e212625d5c32a4187474
Luis Javier Cases <l.cases@cathedralsw.com>
2023-09-07 08:53:36 +0200

    feat(seach-bar): check if className prop is undefined

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 const SearchBar = ({ placeholder, onChange, className }) => {
   return (
     <input
-      className={`w-56 text-sm rounded-lg block p-2.5 border-2 ${className}`}
+      className={`w-56 text-sm rounded-lg block p-2.5 border-2 ${className ?? ''}`}
       type='text'
       onChange={onChange}
       placeholder={placeholder}
