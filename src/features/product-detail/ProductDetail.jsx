51b304ff395053aae645af13dcdddf4d47a6f4d6
Luis Javier Cases <l.cases@cathedralsw.com>
2023-09-07 10:16:14 +0200

    feat(product-detail): add components to page

--- src/features/product-detail/ProductDetail.jsx
+++ src/features/product-detail/ProductDetail.jsx
@@ -1,5 +1,7 @@
 import { useLocation } from 'react-router-dom';
 import { useGetProductDetailQuery } from '@/services/query/productApi';
+import Description from './components/Description';
+import Actions from './components/Actions';
 
 const ProductDetail = () => {
   const { state } = useLocation();
@@ -7,9 +9,19 @@ const ProductDetail = () => {
   const { data: product } = useGetProductDetailQuery(state?.id);
 
   return (
-    <>
-      <span>Current product {product?.model}</span>
-    </>
+    <div className='mx-auto py-8 px-4'>
+      <div className='max-w-screen-lg mx-auto'>
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-4 p-4'>
+          <div className='md:col-span-1'>
+            <img src={product?.imgUrl} alt={product?.name} className='w-56 h-auto' />
+          </div>
+          <div className='md:col-span-1'>
+            <Description product={product} />
+            <Actions className='mt-4' product={product} />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
