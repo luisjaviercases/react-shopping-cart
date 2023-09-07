d55b45dc17a5469ee21c6fef27d83764f0baa930
Luis Javier Cases <l.cases@cathedralsw.com>
2023-09-07 09:16:00 +0200

    style(header): add styles and link to index page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,18 @@
+import { Link } from 'react-router-dom';
 import Breadcrumbs from '@/components/Breadcrumbs/Breadcrumbs';
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
-      <Breadcrumbs />
-    </div>
+    <header className='bg-gray-500 p-4'>
+      <div className='container mx-auto flex justify-between items-center'>
+        <div className='flex flex-col gap-4'>
+          <Link to='/' className='text-white text-2xl font-bold'>
+            Front-End Test
+          </Link>
+          <Breadcrumbs />
+        </div>
+      </div>
+    </header>
   );
 };
 
