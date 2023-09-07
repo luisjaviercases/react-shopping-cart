598de88a504dac5ceb21d7342cabbb92a92a62f4
Luis Javier Cases <l.cases@cathedralsw.com>
2023-09-07 10:44:52 +0200

    feat(product-detail): add goBack button

--- src/features/product-detail/ProductDetail.jsx
+++ src/features/product-detail/ProductDetail.jsx
@@ -2,14 +2,29 @@ import { useLocation } from 'react-router-dom';
 import { useGetProductDetailQuery } from '@/services/query/productApi';
 import Description from './components/Description';
 import Actions from './components/Actions';
+import { usePreviousRoute } from '@/hooks/UsePreviousRoute';
 
 const ProductDetail = () => {
   const { state } = useLocation();
+  const { goBack } = usePreviousRoute();
 
   const { data: product } = useGetProductDetailQuery(state?.id);
 
   return (
-    <div className='mx-auto py-8 px-4'>
+    <div className='mx-auto flex flex-col py-8 px-4 gap-4'>
+      <span
+        className='flex items-center cursor-pointer w-fit'
+        onClick={() => {
+          goBack();
+        }}>
+        <svg
+          className='fill-current stroke-0 h-5 w-5 transform rotate-90'
+          xmlns='http://www.w3.org/2000/svg'
+          viewBox='0 0 20 20'>
+          <path d='M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z' />
+        </svg>
+        Go back
+      </span>
       <div className='max-w-screen-lg mx-auto'>
         <div className='grid grid-cols-1 md:grid-cols-2 gap-4 p-4'>
           <div className='md:col-span-1'>
