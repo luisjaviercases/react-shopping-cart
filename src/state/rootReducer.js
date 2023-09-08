463be893c24565e414786eb5e0a95d92a6750b69
Luis Javier Cases <l.cases@cathedralsw.com>
2023-09-08 09:33:28 +0200

    feat(state): add cart reducer

--- src/state/rootReducer.js
+++ src/state/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import baseApi from '@/services/query/baseApi';
+import { cartReducer } from '@/state/cart/cartSlice';
 
 const rootReducer = combineReducers({
   [baseApi.reducerPath]: baseApi.reducer,
+  cart: cartReducer,
 });
 
 export default rootReducer;
