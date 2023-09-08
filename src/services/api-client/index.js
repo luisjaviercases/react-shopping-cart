c0a9db4db21f88caa247d632e125b1f748b2f5f1
Luis Javier Cases <l.cases@cathedralsw.com>
2023-09-08 09:33:46 +0200

    feat(services): prepare headers in base query

--- src/services/api-client/index.js
+++ src/services/api-client/index.js
@@ -2,6 +2,10 @@ import { fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const apiClient = fetchBaseQuery({
   baseUrl: import.meta.env.VITE_BASE_URL,
+  prepareHeaders: (headers) => {
+    headers.set('Content-Type', 'application/json');
+    return headers;
+  },
 });
 
 export default apiClient;
