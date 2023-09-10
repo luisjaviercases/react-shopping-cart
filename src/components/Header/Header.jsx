cb3e28412dd582c6df25b40e99ef5073f6fa77fb
Luis Javier Cases <l.cases@cathedralsw.com>
2023-09-10 12:36:46 +0200

    style(header): change styles to adapt in small devices

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
 
   return (
     <header className='bg-gray-500 p-4'>
-      <div className='container mx-auto flex justify-between items-center'>
+      <div className='container mx-auto flex flex-col md:flex-row justify-between items-center'>
         <div className='flex flex-col gap-4'>
           <Link to='/' className='text-white text-2xl font-bold'>
             Front-End Test
