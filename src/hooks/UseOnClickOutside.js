ffcd33710b0d93bf19be0632e5f9496f843aba0a
Luis Javier Cases <l.cases@cathedralsw.com>
2023-09-10 13:28:16 +0200

    feat(hooks): add onClickOutside hook
    Call handler only it click is outside of the element passed

---
+++ src/hooks/UseOnClickOutside.js
@@ -0,0 +1,22 @@
+import { useEffect } from 'react';
+
+const useOnClickOutside = (ref, handler) => {
+  useEffect(() => {
+    const listener = (event) => {
+      const el = ref?.current;
+      if (!el || el.contains(event?.target || null)) {
+        return;
+      }
+      // Call the handler only if the click is outside of the element passed.
+      handler(event);
+    };
+
+    document.addEventListener('mousedown', listener);
+
+    return () => {
+      document.removeEventListener('mousedown', listener);
+    };
+  }, [ref, handler]);
+};
+
+export default useOnClickOutside;
