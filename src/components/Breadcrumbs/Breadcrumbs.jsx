7f30447b9ffe6dea6c31748cb64c67de221c7914
Luis Javier Cases <l.cases@cathedralsw.com>
2023-11-21 18:15:09 +0100

    feat(breadcrumbs): change component to consume breadcrumbs from config file

--- src/components/Breadcrumbs/Breadcrumbs.jsx
+++ src/components/Breadcrumbs/Breadcrumbs.jsx
@@ -1,42 +1,51 @@
 import { useLocation, Link } from 'react-router-dom';
+import breadcrumbConfig from '../../config/BreadcrumbsList';
 
 const Breadcrumbs = () => {
   const location = useLocation();
 
-  if (location.pathname === '/404') return;
+  const findBreadcrumbItem = (path) => breadcrumbConfig.find((item) => item.path === path);
 
-  let currentLink = '';
-  let breadCrumbRoutes = location.pathname.split('/').filter((crumb) => crumb !== '');
+  const generateBreadcrumbs = () => {
+    const pathSegments = location.pathname.split('/').filter((crumb) => crumb !== '');
 
-  const crumbs = breadCrumbRoutes.map((crumb, index) => {
-    currentLink += `/${crumb}`;
+    return pathSegments.map((segment, index) => {
+      const currentPath = `/${pathSegments.slice(0, index + 1).join('/')}`;
+      const breadcrumbItem = findBreadcrumbItem(currentPath);
 
-    return (
-      <li key={crumb} className='flex items-center'>
-        {breadCrumbRoutes[breadCrumbRoutes.length - 1] === crumb ? (
-          <span className='text-white'>{crumb}</span>
-        ) : (
-          <Link to={currentLink} className='text-white hover:underline'>
-            {crumb}
-          </Link>
-        )}
-        {index < breadCrumbRoutes.length - 1 && (
-          <span className='mx-1 text-white'>
-            <svg
-              className='fill-current stroke-0 h-3.5 w-3.5 transform -rotate-90'
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 20 20'>
-              <path d='M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z' />
-            </svg>
-          </span>
-        )}
-      </li>
-    );
-  });
+      return (
+        <li key={currentPath} className='flex items-center'>
+          {breadcrumbItem ? (
+            <>
+              {breadcrumbItem.path === currentPath ? (
+                <span className='text-white'>{breadcrumbItem.label}</span>
+              ) : (
+                <Link to={currentPath} className='text-white hover:underline'>
+                  {breadcrumbItem.label}
+                </Link>
+              )}
+            </>
+          ) : (
+            <span className='text-white'>{segment}</span>
+          )}
+          {index < pathSegments.length - 1 && (
+            <span className='mx-1 text-white'>
+              <svg
+                className='fill-current stroke-0 h-3.5 w-3.5 transform -rotate-90'
+                xmlns='http://www.w3.org/2000/svg'
+                viewBox='0 0 20 20'>
+                <path d='M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z' />
+              </svg>
+            </span>
+          )}
+        </li>
+      );
+    });
+  };
 
   return (
     <nav>
-      <ul className='flex'>{crumbs}</ul>
+      <ul className='flex'>{generateBreadcrumbs()}</ul>
     </nav>
   );
 };
