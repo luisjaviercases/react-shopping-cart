8a6211a7d918a597a9e65fc048480bb4b973c744
Luis Javier Cases <l.cases@cathedralsw.com>
2023-11-24 13:32:44 +0100

    feat(products): remove unnecessary optional operators
    Using loader while product detail is being loaded make unnecessary the use of this operators

--- src/features/products/Products.jsx
+++ src/features/products/Products.jsx
@@ -27,7 +27,7 @@ const Products = () => {
         <LoadingSpinner />
       ) : (
         <div className='grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-          {filteredProducts?.map((product) => (
+          {filteredProducts.map((product) => (
             <ItemList key={product.id} product={product} />
           ))}
         </div>
