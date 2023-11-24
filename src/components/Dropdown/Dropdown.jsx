e0ab28c9af577b89d1288d158cee6f34432d72df
Luis Javier Cases <l.cases@cathedralsw.com>
2023-11-24 16:28:10 +0100

    feat(dropdown): make componet accessible

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -15,6 +15,15 @@ const Dropdown = ({ label, options, onSelect, selectedOption }) => {
     onSelect(option);
   };
 
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter' || event.key === ' ') {
+      toggleDropdown();
+    } else if (event.key === 'ArrowDown') {
+      event.preventDefault();
+      setIsOpen(true);
+    }
+  };
+
   useOnClickOutside(dropdownRef, () => {
     if (isOpen) {
       setIsOpen(false);
@@ -29,7 +38,11 @@ const Dropdown = ({ label, options, onSelect, selectedOption }) => {
           type='button'
           className='inline-flex justify-center items-center w-full rounded-md
             border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700'
-          onClick={toggleDropdown}>
+          onClick={toggleDropdown}
+          onKeyDown={handleKeyDown}
+          aria-haspopup='listbox'
+          aria-expanded={isOpen}
+          aria-labelledby='dropdown-label'>
           {selectedOption ? selectedOption.label : 'Select an option'}
           <svg className='fill-current stroke-0 h-3.5 w-3.5' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'>
             <path d='M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z' />
@@ -39,7 +52,9 @@ const Dropdown = ({ label, options, onSelect, selectedOption }) => {
         {isOpen && (
           <div
             className='origin-top-right absolute right-0 mt-2 w-full rounded-md shadow-lg 
-          bg-white ring-1 ring-black ring-opacity-5 focus:outline-none'>
+          bg-white ring-1 ring-black ring-opacity-5 focus:outline-none'
+            role='listbox'
+            id='dropdown-label'>
             {options?.map((option) => (
               <button
                 key={`${option.value}-${option.label}`}
@@ -48,7 +63,10 @@ const Dropdown = ({ label, options, onSelect, selectedOption }) => {
                   selectedOption && selectedOption.value === option.value
                     ? 'bg-gray-100 text-gray-900'
                     : 'text-gray-700'
-                } block px-4 py-2 text-sm w-full text-left hover:bg-gray-100 hover:text-gray-900`}>
+                } block px-4 py-2 text-sm w-full text-left hover:bg-gray-100 hover:text-gray-900`}
+                role='option'
+                aria-selected={selectedOption && selectedOption.value === option.value}
+                tabIndex={isOpen ? 0 : -1}>
                 {option.label}
               </button>
             ))}
